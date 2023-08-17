1af246c61b859982dd4fda3ea4f88e046d891a88
Brandon Ros <brandonros1@gmail.com>
2023-08-16 20:28:55 -0400

    threaded blink

--- rp2040-serial-hci/src/main.rs
+++ rp2040-serial-hci/src/main.rs
@@ -2,37 +2,42 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-mod led_state;
 mod pins;
+mod led_state;
 
-use defmt_serial as _;
-use embassy_executor::Executor;
-use embassy_rp::{
-    bind_interrupts,
-    gpio::{self, AnyPin, Level, Pin},
-    peripherals::UART0,
-    uart::{self, InterruptHandler},
-};
-use embassy_sync::{
-    blocking_mutex::raw::ThreadModeRawMutex,
-    channel::{Channel, Receiver, Sender},
-};
-
-use embassy_time::{Timer, Duration};
+use cyw43::Control;
+use cyw43_pio::PioSpi;
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Level, Output, AnyPin, self};
+use embassy_rp::peripherals::{DMA_CH0, PIN_23, PIN_25, PIO0, UART0};
+use embassy_rp::pio::Pio;
+use embassy_rp::{bind_interrupts, uart};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::channel::{Receiver, Sender, Channel};
+use embassy_time::{Duration, Timer};
+use led_state::LedState;
 use panic_probe as _;
-use static_cell::StaticCell;
-
-use crate::led_state::LedState;
+use static_cell::make_static;
 
 bind_interrupts!(struct Irqs {
-    UART0_IRQ => InterruptHandler<UART0>;
+    UART0_IRQ => embassy_rp::uart::InterruptHandler<UART0>;
+    PIO0_IRQ_0 => embassy_rp::pio::InterruptHandler<PIO0>;
 });
 
-static CORE0_EXECUTOR: StaticCell<Executor> = StaticCell::new();
-static LED_CHANNEL: StaticCell<Channel<ThreadModeRawMutex, LedState, 1>> = StaticCell::new();
+#[embassy_executor::task]
+async fn wifi_task(
+    runner: cyw43::Runner<
+        'static,
+        Output<'static, PIN_23>,
+        PioSpi<'static, PIN_25, PIO0, 0, DMA_CH0>,
+    >,
+) -> ! {
+    runner.run().await
+}
 
-#[cortex_m_rt::entry]
-fn main() -> ! {
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
     // UART0
     let uart0 = uart::Uart::new(
@@ -40,38 +45,49 @@ fn main() -> ! {
         pins::get_uart0_tx_pin!(p),
         pins::get_uart0_rx_pin!(p),
         Irqs,      // unused?
-        p.DMA_CH0, // unused?
         p.DMA_CH1, // unused?
+        p.DMA_CH2, // unused?
         uart::Config::default(),
     );
     // defmt serial
     defmt_serial::defmt_serial(uart0);
-    // log
-    defmt::info!("init");
+    // firmware
+    let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
+    let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
+    // cyw43 init
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
+    );
+    let state = make_static!(cyw43::State::new());
+    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    unwrap!(spawner.spawn(wifi_task(runner)));
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
     // LED channel
-    let led_channel = LED_CHANNEL.init(Channel::new());
+    let led_channel = make_static!(Channel::new());
     let led_channel_receiver = led_channel.receiver();
     let led_channel_sender = led_channel.sender();
-    // spawn tasks (core0)
-    let core0_executor = CORE0_EXECUTOR.init(Executor::new());
-    core0_executor.run(|spawner| {
-        spawner.must_spawn(led_task(
-            pins::get_led_pin!(p).degrade(),
-            led_channel_receiver,
-        ));
-        spawner.must_spawn(blink_task(
-            led_channel_sender
-        ));
-    });
+    unwrap!(spawner.spawn(led_task(control, led_channel_receiver)));
+    unwrap!(spawner.spawn(blink_task(led_channel_sender)));
 }
 
 #[embassy_executor::task]
-async fn led_task(led_pin: AnyPin, receiver: Receiver<'static, ThreadModeRawMutex, LedState, 1>) {
-    let mut led_output = gpio::Output::new(led_pin, Level::Low);
+async fn led_task(mut control: Control<'static>, receiver: Receiver<'static, ThreadModeRawMutex, LedState, 1>) {
     loop {
         match receiver.recv().await {
-            LedState::On => led_output.set_high(),
-            LedState::Off => led_output.set_low(),
+            LedState::On => control.gpio_set(0, true).await,
+            LedState::Off => control.gpio_set(0, false).await,
         }
     }
 }
@@ -80,8 +96,8 @@ async fn led_task(led_pin: AnyPin, receiver: Receiver<'static, ThreadModeRawMute
 async fn blink_task(led_sender: Sender<'static, ThreadModeRawMutex, LedState, 1>) {
     loop {
         led_sender.send(LedState::On).await;
-        Timer::after(Duration::from_millis(100)).await;
+        Timer::after(Duration::from_millis(1000)).await;
         led_sender.send(LedState::Off).await;
-        Timer::after(Duration::from_millis(5)).await;
+        Timer::after(Duration::from_millis(1000)).await;
     }
 }
--- rp2040-serial-hci/src/pins.rs
+++ rp2040-serial-hci/src/pins.rs
@@ -3,7 +3,3 @@ macro_rules! get_uart0_tx_pin { ($p:expr) => { $p.PIN_16 }; } // 21, UART0 TX (b
 macro_rules! get_uart0_rx_pin { ($p:expr) => { $p.PIN_17 }; } // 22, UART0 RX (white)
 pub(crate) use get_uart0_tx_pin;
 pub(crate) use get_uart0_rx_pin;
-
-// LED
-macro_rules! get_led_pin { ($p:expr) => { $p.PIN_25 }; } // built in
-pub(crate) use get_led_pin;
