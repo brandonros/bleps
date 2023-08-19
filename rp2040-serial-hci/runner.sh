9069fbab8c5e5608f832766856733a7cd77218c8
Brandon Ros <brandonros1@gmail.com>
2023-08-19 13:27:17 -0400

    going different direction

--- rp2040-serial-hci/runner.sh
+++ rp2040-serial-hci/runner.sh
@@ -6,4 +6,6 @@ set -e
 elf2uf2-rs -d "$1"
 
 # debug uart
-/Users/brandon/Desktop/defmt/target/debug/defmt-print -e "$1" serial --path /dev/tty.usbserial-A50285BI
+#/Users/brandon/Desktop/defmt/target/debug/defmt-print -e "$1" serial --path /dev/tty.usbserial-A50285BI
+# (stty speed 115200 >/dev/null && cat) < /dev/tty.usbserial-A50285BI
+(stty speed 115200 >/dev/null && cat) < /dev/cu.usbserial-A50285BI | defmt-print -e "$1"
